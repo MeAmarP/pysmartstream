cec43233e04874ab3bfb27e13f8f2f964740ef4b
MeAmarP <appotdar05@gmail.com>
2024-04-01 23:52:18 +0530

    updated for mylib

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import cv2
 from flask import Flask, render_template, Response
 
 from camera import VideoCamera, Color
-from objectdetector import ObjectDetector
+from detector import ObjectDetector
 
 
 # TODO: Single Loggerclass to debug them all.
@@ -14,35 +14,37 @@ from objectdetector import ObjectDetector
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--path_to_model_config", help="Provide Path to DNN model config file.")
-parser.add_argument("--path_to_model_weights", help="Provide Path to DNN model weights file.")
+parser.add_argument("--video_file", help="Provide Path to Video file.")
+parser.add_argument("--model_config", help="Provide Path to DNN model config file.")
+parser.add_argument("--model_weights", help="Provide Path to DNN model weights file.")
+
 args = parser.parse_args()
 
 app = Flask(__name__)
 
-if platform.machine() == 'aarch64':
-    src = 'v4l2src ! video/x-raw,width={},height={} ! videoconvert ! appsink'.format(640, 480)
-else:
-    src = 0
-# Init video stream with source <Local Camera = 0 | Video file>, frame Width & Height
-camera = VideoCamera(src=src, width=640, height=480)
-
-ssd_mdl = ObjectDetector(args.path_to_model_weights, args.path_to_model_config)
+camera = cv2.VideoCapture(args.video_file)  # Video File Source
+yolo_detector = ObjectDetector(model_cfg=args.model_config, 
+                     model_weights= args.model_weights,
+                     class_file="./assets/coco.names")
 
 
 def process_frames(cam):
     while True:
-        ret, myframe = cam.video.read()
-        objclass_values, objscore_values, objbboxes_values = ssd_mdl.detect_objects_in_frame(myframe)
-        for objclass_ele, score_ele,bbox_ele in zip(objclass_values, objscore_values, objbboxes_values):
-            cv2.rectangle(myframe,
-                          (int(bbox_ele[0]), int(bbox_ele[1])),(int(bbox_ele[2] + bbox_ele[0]), int(bbox_ele[3] + bbox_ele[1])),
-                          Color.yellow,
-                          thickness=2)
-        cam.draw_str(myframe,(20, 700),"Hello-World")
-        frame = cam.encode_frame(myframe)
+        ret, myframe = cam.read()
+        # Check if the frame is valid
+        if not ret or myframe is None:
+            print("Error: Please provide a valid video file.")
+            break
+        detections = yolo_detector.detect(myframe, target_class_id=0) # 0 for person
+
+        if len(detections) > 0:
+            yolo_detector.display_detections(frame=myframe, detections=detections)
+
+        _, frame_jpeg = cv2.imencode('.jpg', myframe)
+        frame_jpeg = frame_jpeg.tobytes()
+        # frame = cam.encode_frame(myframe)
         yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+               b'Content-Type: image/jpeg\r\n\r\n' + frame_jpeg + b'\r\n')
 
 # =============================================================================
 # App Functions
@@ -58,6 +60,8 @@ def video_feed():
 
 
 if __name__ == '__main__':
+#/home/c3po/Documents/project/learning/amar-works/yolo-triton-onnx/models/yolov3.cfg
+#/home/c3po/Documents/project/learning/amar-works/yolo-triton-onnx/models/yolov3.weights
     host_ip = "127.0.0.1"
     # host_ip = "0.0.0.0"
     app.run(host=host_ip, debug=True, port=8888)
